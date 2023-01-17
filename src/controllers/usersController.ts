36e416fdb92a57c38050f402e04f6a2fbbdec340
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2023-01-17 11:17:13 +0100

    pull

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,52 +1,48 @@
-import { Express, Request, Response, Router } from "express";
+import { Express, Request, Response, Router } from 'express';
 import * as bcrypt from 'bcrypt';
-import client from "../constant/client";
-import { UsersServices } from "../services/usersServices";
-
+import client from '../constant/client';
+import { UsersServices } from '../services/usersServices';
 
 const usersServices = new UsersServices();
 
 export class UsersController {
-
     async register(req: Request, res: Response) {
         try {
             const name: string = req.body.name;
             const password: string = req.body.password;
 
-            const listNames: string[] | undefined = await usersServices.getNames();
-
+            const listNames: string[] | undefined =
+                await usersServices.getNames();
 
             if ((name && password) !== undefined) {
                 if (listNames!.includes(name)) {
                     res.status(400).json({
-                        status: "FAIL",
-                        message: `Le nom ${name} existe déjà !!`
-                    })
-                    return
+                        status: 'FAIL',
+                        message: `Le nom ${name} existe déjà !!`,
+                    });
+                    return;
                 }
                 bcrypt.hash(password, 10, async function (err, hash) {
                     const registerOk = await usersServices.addUser(name, hash);
 
                     res.status(200).json({
-                        status: "OK",
+                        status: 'OK',
                         message: `Nous vous avons bien enregistré, ${registerOk} !!`,
-                        data: { name: registerOk }
-                    })
-                })
-            }
-            else {
+                        data: { name: registerOk },
+                    });
+                });
+            } else {
                 res.status(400).json({
-                    status: "FAIL",
-                    message: "Vérifiez vos données saisies !!"
-                })
+                    status: 'FAIL',
+                    message: 'Vérifiez vos données saisies !!',
+                });
             }
-
         } catch (error) {
             console.log(error);
             res.status(404).json({
-                status: "ERROR",
-                message: "!!! ERREUR !!!"
-            })
+                status: 'ERROR',
+                message: '!!! ERREUR !!!',
+            });
         }
     }
 
@@ -55,45 +51,42 @@ export class UsersController {
             const name: string = req.body.name;
             const password: string = req.body.password;
 
-            const data = await usersServices.getDataUserbyName(name)
+            const data = await usersServices.getDataUserbyName(name);
 
-            if (name === undefined || password  === undefined) {
+            if (name === undefined || password === undefined) {
                 return res.status(400).json({
-                    status: "FAIL",
-                    message: `Données manquantes !!`
-                })
+                    status: 'FAIL',
+                    message: `Données manquantes !!`,
+                });
             }
             if (data) {
-
                 const hash = data.password;
 
                 bcrypt.compare(password, hash, async (err, result) => {
-
                     if (result) {
                         res.status(200).json({
-                            status: "OK",
-                            message: `Vous êtes bien connecté.e !!`
-                        })
+                            status: 'OK',
+                            message: `Vous êtes bien connecté.e !!`,
+                        });
                     } else {
                         res.status(401).json({
-                            status: "FAIL",
-                            message: `Le mot de passe de corresponnd pas !!`
-                        })
+                            status: 'FAIL',
+                            message: `Le mot de passe de corresponnd pas !!`,
+                        });
                     }
-                })
+                });
             } else {
                 res.status(400).json({
-                    status: "FAIL",
-                    message: `${name} n'a pas de compte !!`
-                })
+                    status: 'FAIL',
+                    message: `${name} n'a pas de compte !!`,
+                });
             }
         } catch (error) {
             console.log(error);
             res.status(500).json({
-                status: "ERROR",
-                message: `!!! ERREUR !!!`
-            })
+                status: 'ERROR',
+                message: `!!! ERREUR !!!`,
+            });
         }
-
     }
 }
--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -1,38 +1,42 @@
-import { QueryResult, QueryResultRow } from "pg";
-import client from "../constant/client";
-import TUsers from "../types/TUsers";
+import { QueryResult, QueryResultRow } from 'pg';
+import client from '../constant/client';
+import TUsers from '../types/TUsers';
 import * as bcrypt from 'bcrypt';
 
 export class UsersServices {
-
-    async getNames() : Promise<string[] | undefined>{
-        
-        const data : QueryResult<TUsers> = await client.query('select name from users');
-        
-        if (data.rowCount) {
-            const names : string[] = data.rows.map(data => data.name);
-            return names
-        }
-        return undefined
-    };
-
-    async addUser(name : string,password: string) : Promise<string | undefined>{
-
-        const data : QueryResult<TUsers> = await client.query('insert into users (name,password) values ($1,$2) returning *',[name,password]);
+    async getNames(): Promise<string[] | undefined> {
+        const data: QueryResult<TUsers> = await client.query(
+            'select name from users'
+        );
 
         if (data.rowCount) {
-            return data.rows[0].name
+            const names: string[] = data.rows.map((data) => data.name);
+            return names;
         }
-        return undefined
+        return undefined;
     }
 
-    async getDataUserbyName(name : string) : Promise<TUsers | undefined>{
+    async addUser(name: string, password: string): Promise<string | undefined> {
+        const data: QueryResult<TUsers> = await client.query(
+            'insert into users (name,password) values ($1,$2) returning *',
+            [name, password]
+        );
 
-        const data : QueryResult<TUsers> = await client.query('select * from users where name = $1', [name]);
-        
         if (data.rowCount) {
-            return data.rows[0]
+            return data.rows[0].name;
         }
-        return undefined
+        return undefined;
+    }
+
+    async getDataUserbyName(name: string): Promise<TUsers | undefined> {
+        const data: QueryResult<TUsers> = await client.query(
+            'select * from users where name = $1',
+            [name]
+        );
+
+        if (data.rowCount) {
+            return data.rows[0];
+        }
+        return undefined;
     }
 }
