48cb5bd7b22d330d15060fa5cb67cab206dd6daf
WAM <eraserw@gmail.com>
2023-01-17 11:59:17 +0100

    feat token

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,99 +1,119 @@
-import { Express, Request, Response, Router } from "express";
+import { Express, Request, Response, Router } from 'express';
 import * as bcrypt from 'bcrypt';
-import client from "../constant/client";
-import { UsersServices } from "../services/usersServices";
-
+import * as jwt from 'jsonwebtoken';
+import client from '../constant/client';
+import { UsersServices } from '../services/usersServices';
+import { RequestWithUserRole } from '../types/TToken';
 
 const usersServices = new UsersServices();
 
+const secreToken = process.env.secreToken!;
+
 export class UsersController {
+        async register(req: Request, res: Response) {
+                try {
+                        const name: string = req.body.name;
+                        const password: string = req.body.password;
 
-    async register(req: Request, res: Response) {
-        try {
-            const name: string = req.body.name;
-            const password: string = req.body.password;
+                        const listNames: string[] | undefined =
+                                await usersServices.getNames();
 
-            const listNames: string[] | undefined = await usersServices.getNames();
+                        if ((name && password) !== undefined) {
+                                if (listNames!.includes(name)) {
+                                        res.status(400).json({
+                                                status: 'FAIL',
+                                                message: `Le nom ${name} existe déjà !!`,
+                                        });
+                                        return;
+                                }
+                                bcrypt.hash(
+                                        password,
+                                        10,
+                                        async function (err, hash) {
+                                                const registerOk =
+                                                        await usersServices.addUser(
+                                                                name,
+                                                                hash
+                                                        );
 
-
-            if ((name && password) !== undefined) {
-                if (listNames!.includes(name)) {
-                    res.status(400).json({
-                        status: "FAIL",
-                        message: `Le nom ${name} existe déjà !!`
-                    })
-                    return
+                                                res.status(200).json({
+                                                        status: 'OK',
+                                                        message: `Nous vous avons bien enregistré, ${registerOk} !!`,
+                                                        data: {
+                                                                name: registerOk,
+                                                        },
+                                                });
+                                        }
+                                );
+                        } else {
+                                res.status(400).json({
+                                        status: 'FAIL',
+                                        message: 'Vérifiez vos données saisies !!',
+                                });
+                        }
+                } catch (error) {
+                        console.log(error);
+                        res.status(404).json({
+                                status: 'ERROR',
+                                message: '!!! ERREUR !!!',
+                        });
                 }
-                bcrypt.hash(password, 10, async function (err, hash) {
-                    const registerOk = await usersServices.addUser(name, hash);
-
-                    res.status(200).json({
-                        status: "OK",
-                        message: `Nous vous avons bien enregistré, ${registerOk} !!`,
-                        data: { name: registerOk }
-                    })
-                })
-            }
-            else {
-                res.status(400).json({
-                    status: "FAIL",
-                    message: "Vérifiez vos données saisies !!"
-                })
-            }
-
-        } catch (error) {
-            console.log(error);
-            res.status(404).json({
-                status: "ERROR",
-                message: "!!! ERREUR !!!"
-            })
-        }
-    }
-
-    async login(req: Request, res: Response) {
-        try {
-            const name: string = req.body.name;
-            const password: string = req.body.password;
-
-            const data = await usersServices.getDataUserbyName(name)
-
-            if (name === undefined || password  === undefined) {
-                return res.status(400).json({
-                    status: "FAIL",
-                    message: `Données manquantes !!`
-                })
-            }
-            if (data) {
-
-                const hash = data.password;
-
-                bcrypt.compare(password, hash, async (err, result) => {
-
-                    if (result) {
-                        res.status(200).json({
-                            status: "OK",
-                            message: `Vous êtes bien connecté.e !!`
-                        })
-                    } else {
-                        res.status(401).json({
-                            status: "FAIL",
-                            message: `Le mot de passe de corresponnd pas !!`
-                        })
-                    }
-                })
-            } else {
-                res.status(400).json({
-                    status: "FAIL",
-                    message: `${name} n'a pas de compte !!`
-                })
-            }
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({
-                status: "ERROR",
-                message: `!!! ERREUR !!!`
-            })
         }
 
-    }
+        async login(req: Request, res: Response) {
+                try {
+                        const name: string = req.body.name;
+                        const password: string = req.body.password;
+
+                        const data = await usersServices.getDataUserbyName(
+                                name
+                        );
+
+                        if (name === undefined || password === undefined) {
+                                return res.status(400).json({
+                                        status: 'FAIL',
+                                        message: `Données manquantes !!`,
+                                });
+                        }
+                        if (data) {
+                                const hash = data.password;
+
+                                bcrypt.compare(
+                                        password,
+                                        hash,
+                                        async (err, result) => {
+                                                const id = data.id;
+                                                const token = jwt.sign(
+                                                        { id },
+                                                        secreToken
+                                                );
+
+                                                if (result) {
+                                                        res.status(200).json({
+                                                                status: 'OK',
+                                                                message: `Vous êtes bien connecté.e !!`,
+                                                                token: token,
+                                                        });
+                                                } else {
+                                                        res.status(401).json({
+                                                                status: 'FAIL',
+                                                                message: `Le mot de passe de corresponnd pas !!`,
+                                                        });
+                                                }
+                                        }
+                                );
+                        } else {
+                                res.status(400).json({
+                                        status: 'FAIL',
+                                        message: `${name} n'a pas de compte !!`,
+                                });
+                        }
+                } catch (error) {
+                        console.log(error);
+                        res.status(500).json({
+                                status: 'ERROR',
+                                message: `!!! ERREUR !!!`,
+                        });
+                }
+        }
 }
--- src/routes/articlesRouter.ts
+++ src/routes/articlesRouter.ts
@@ -1,20 +1,19 @@
-import { Router } from "express";
-import { QueryResult } from "pg";
-import client from "../constant/client";
-import { ArticlesController } from "../controllers/articlesController";
-import { TArticles } from "../types/TArticles";
-
+import { Router } from 'express';
+import { QueryResult } from 'pg';
+import client from '../constant/client';
+import { ArticlesController } from '../controllers/articlesController';
+import { verifyToken } from '../middleware/auth';
+import { TArticles } from '../types/TArticles';
 
 const articlesRouter = Router();
-const articlesController = new ArticlesController;
+const articlesController = new ArticlesController();
 
 // changer la valeur de l'id au niveau de articlesServices ligne 33 !!!!!!
 
-articlesRouter.get('/', articlesController.getAllArticles);
+articlesRouter.get('/', verifyToken, articlesController.getAllArticles);
 
 articlesRouter.get('/:id', articlesController.getOneArticle);
 
 articlesRouter.post('/', articlesController.postArticle);
-
 
 export default articlesRouter;
