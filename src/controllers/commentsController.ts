3c140d362d56557db49f1d58b3263c379ec7a332
WAM <eraserw@gmail.com>
2023-01-19 13:51:27 +0100

    pull 190123-13h51

--- src/controllers/commentsController.ts
+++ src/controllers/commentsController.ts
@@ -10,215 +10,221 @@ const commentsServices = new CommentsServices();
  * * **.putComment()** : Contrôle préalable à la modification d'un commentaire
  */
 export class CommentsController {
-    /**
-     * Vérification de l'existence d'article et leur commentaires
-     * * Response.data retourne les commentaires liés à l'article
-     */
-    async getArticleWithComments(req: Request, res: Response) {
-        const articleId: string = req.params.id;
+        /**
+         * Vérification de l'existence d'article et leur commentaires
+         * * Response.data retourne les commentaires liés à l'article
+         */
+        async getArticleWithComments(req: Request, res: Response) {
+                const articleId: string = req.params.id;
 
-        try {
-            const dataArticle = await commentsServices.getAllCommentsbyArticle(
-                articleId
-            );
+                try {
+                        const dataArticle =
+                                await commentsServices.getAllCommentsbyArticle(
+                                        articleId
+                                );
 
-            if (dataArticle) {
-                res.status(200).json({
-                    status: 'SUCCESS',
-                    message: `voici tous les commentaire de l'article ${articleId}`,
-                    data: dataArticle,
-                });
-            } else {
-                res.status(404).json({
-                    status: 'FAIL',
-                    message: `Pas d'article numéro : ${articleId}`,
-                    data: null,
-                });
-            }
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({
-                status: 'ERROR',
-                message: 'erreur serveur',
-                data: null,
-            });
-        }
-    }
-
-    /**
-     * Vérification des données en entré avant création
-     * * Response.data retourne le commentaire crée
-     */
-    async postComment(req: Request, res: Response) {
-        const user_id = req.body.user_id;
-        const content = req.body.content;
-        const article_id = req.params.id;
-
-        if (article_id === undefined || content === undefined) {
-            res.status(400).json({
-                status: 'FAIL',
-                message: 'valeur manquante',
-                data: null,
-            });
-        } else {
-            try {
-                const dataComment = await commentsServices.addComment(
-                    user_id,
-                    content,
-                    article_id
-                );
-
-                if (dataComment === undefined) {
-                    res.status(404).json({
-                        status: 'FAIL',
-                        message: 'Aucun commentaire trouvé',
-                        data: null,
-                    });
-                } else {
-                    res.status(200).json({
-                        status: 'SUCCESS',
-                        message: 'Commentaire publié !',
-                        data: dataComment,
-                    });
+                        if (dataArticle) {
+                                res.status(200).json({
+                                        status: 'SUCCESS',
+                                        message: `voici tous les commentaire de l'article ${articleId}`,
+                                        data: dataArticle,
+                                });
+                        } else {
+                                res.status(404).json({
+                                        status: 'FAIL',
+                                        message: `Pas d'article numéro : ${articleId}`,
+                                        data: null,
+                                });
+                        }
+                } catch (error) {
+                        console.log(error);
+                        res.status(500).json({
+                                status: 'ERROR',
+                                message: 'erreur serveur',
+                                data: null,
+                        });
                 }
-            } catch (err) {
-                console.log(err);
-                res.status(500).json({
-                    status: 'ERROR',
-                    message: 'erreur serveur',
-                    data: null,
-                });
-            }
-        }
-    }
-
-    /**
-     * Vérification de l'existence du commentaire et des données entrante avant création
-     * * Response.data retourne le commentaires modifié
-     */
-    async putComment(req: Request, res: Response) {
-        const user_id = req.body.user_id;
-        const content = req.body.content;
-        const comment_id = req.params.id;
-
-        if (Number.isNaN(Number(comment_id))) {
-            return res.status(404).json({
-                status: 'FAIL',
-                message:
-                    'Type de donnée attendu incorrect, type attendu Number',
-                data: null,
-            });
-        }
-        if (content === undefined) {
-            return res.status(400).json({
-                status: 'FAIL',
-                message: 'valeur manquante',
-                data: null,
-            });
         }
 
-        try {
-            const dataComment = await commentsServices.getComment(comment_id);
+        /**
+         * Vérification des données en entré avant création
+         * * Response.data retourne le commentaire crée
+         */
+        async postComment(req: Request, res: Response) {
+                const user_id = req.body.user_id;
+                const content = req.body.content;
+                const article_id = req.params.id;
 
-            if (!dataComment) {
-                res.status(404).json({
-                    status: 'FAIL',
-                    message: 'Aucun commentaire ne correspond à cet id',
-                    data: null,
-                });
-                return;
-            }
+                if (article_id === undefined || content === undefined) {
+                        res.status(400).json({
+                                status: 'FAIL',
+                                message: 'valeur manquante',
+                                data: null,
+                        });
+                } else {
+                        try {
+                                const dataComment =
+                                        await commentsServices.addComment(
+                                                user_id,
+                                                content,
+                                                article_id
+                                        );
 
-            if (user_id !== dataComment.user_id) {
-                res.status(403).json({
-                    status: 'fail',
-                    message: "Vous n'avez pas accès à ce commentaire",
-                    data: null,
-                });
-                return;
-            }
-
-            const dataUpdated = await commentsServices.updateComment(
-                user_id,
-                content,
-                comment_id
-            );
-
-            if (dataUpdated !== undefined) {
-                res.status(201).json({
-                    status: 'success',
-                    message: 'commentaire modifié',
-                    data: dataUpdated,
-                });
-            }
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({
-                status: 'FAIL',
-                message: 'erreur serveur',
-                data: null,
-            });
-        }
-    }
-
-    /**
-     * Vérification de l'existence du commentaire et suppression
-     * * Response.data retourne message confirmation de suppression
-     */
-    async deleteComment(req: Request, res: Response) {
-        const user_id: string = req.body.user_id;
-        const comment_id: string = req.params.id;
-
-        if (Number.isNaN(Number(comment_id))) {
-            return res.status(404).json({
-                status: 'FAIL',
-                message:
-                    'Type de donnée attendu incorrect, type attendu Number',
-                data: null,
-            });
+                                if (dataComment === undefined) {
+                                        res.status(404).json({
+                                                status: 'FAIL',
+                                                message: 'Aucun commentaire trouvé',
+                                                data: null,
+                                        });
+                                } else {
+                                        res.status(200).json({
+                                                status: 'SUCCESS',
+                                                message: 'Commentaire publié !',
+                                                data: dataComment,
+                                        });
+                                }
+                        } catch (err) {
+                                console.log(err);
+                                res.status(500).json({
+                                        status: 'ERROR',
+                                        message: 'erreur serveur',
+                                        data: null,
+                                });
+                        }
+                }
         }
 
-        try {
-            const dataComment = await commentsServices.getComment(comment_id);
+        /**
+         * Vérification de l'existence du commentaire et des données entrante avant création
+         * * Response.data retourne le commentaires modifié
+         */
+        async putComment(req: Request, res: Response) {
+                const user_id = req.body.user_id;
+                const content = req.body.content;
+                const comment_id = req.params.id;
 
-            if (!dataComment) {
-                res.status(404).json({
-                    status: 'FAIL',
-                    message: 'Aucun commentaire ne correspond à cet id',
-                    data: null,
-                });
-                return;
-            }
+                if (Number.isNaN(Number(comment_id))) {
+                        return res.status(404).json({
+                                status: 'FAIL',
+                                message: 'Type de donnée attendu incorrect, type attendu Number',
+                                data: null,
+                        });
+                }
+                if (content === undefined) {
+                        return res.status(400).json({
+                                status: 'FAIL',
+                                message: 'valeur manquante',
+                                data: null,
+                        });
+                }
 
-            if (user_id !== dataComment.user_id) {
-                res.status(403).json({
-                    status: 'fail',
-                    message: "Vous n'avez pas accès à ce commentaire",
-                    data: null,
-                });
-                return;
-            }
+                try {
+                        const dataComment = await commentsServices.getComment(
+                                comment_id
+                        );
 
-            const dataArchived = await commentsServices.deleteComment(
-                user_id,
-                comment_id
-            );
-            console.log(dataArchived);
+                        if (!dataComment) {
+                                res.status(404).json({
+                                        status: 'FAIL',
+                                        message: 'Aucun commentaire ne correspond à cet id',
+                                        data: null,
+                                });
+                                return;
+                        }
 
-            if (dataArchived) {
-                res.status(201).json({
-                    status: 'success',
-                    message: 'commentaire supprimé !!',
-                    data: dataArchived,
-                });
-            }
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({
-                status: 'FAIL',
-                message: 'erreur serveur',
-                data: null,
-            });
+                        if (user_id !== dataComment.user_id) {
+                                res.status(403).json({
+                                        status: 'fail',
+                                        message: "Vous n'avez pas accès à ce commentaire",
+                                        data: null,
+                                });
+                                return;
+                        }
+
+                        const dataUpdated =
+                                await commentsServices.updateComment(
+                                        user_id,
+                                        content,
+                                        comment_id
+                                );
+
+                        if (dataUpdated !== undefined) {
+                                res.status(201).json({
+                                        status: 'success',
+                                        message: 'commentaire modifié',
+                                        data: dataUpdated,
+                                });
+                        }
+                } catch (err) {
+                        console.log(err);
+                        res.status(500).json({
+                                status: 'FAIL',
+                                message: 'erreur serveur',
+                                data: null,
+                        });
+                }
         }
-    }
+
+        /**
+         * Vérification de l'existence du commentaire et suppression
+         * * Response.data retourne message confirmation de suppression
+         */
+        async deleteComment(req: Request, res: Response) {
+                const user_id: string = req.body.user_id;
+                const comment_id: string = req.params.id;
+
+                if (Number.isNaN(Number(comment_id))) {
+                        return res.status(404).json({
+                                status: 'FAIL',
+                                message: 'Type de donnée attendu incorrect, type attendu Number',
+                                data: null,
+                        });
+                }
+
+                try {
+                        const dataComment = await commentsServices.getComment(
+                                comment_id
+                        );
+
+                        if (!dataComment) {
+                                res.status(404).json({
+                                        status: 'FAIL',
+                                        message: 'Aucun commentaire ne correspond à cet id',
+                                        data: null,
+                                });
+                                return;
+                        }
+
+                        if (user_id !== dataComment.user_id) {
+                                res.status(403).json({
+                                        status: 'fail',
+                                        message: "Vous n'avez pas accès à ce commentaire",
+                                        data: null,
+                                });
+                                return;
+                        }
+
+                        const dataArchived =
+                                await commentsServices.deleteComment(
+                                        user_id,
+                                        comment_id
+                                );
+                        console.log(dataArchived);
+
+                        if (dataArchived) {
+                                res.status(201).json({
+                                        status: 'success',
+                                        message: 'commentaire supprimé !!',
+                                        data: dataArchived,
+                                });
+                        }
+                } catch (err) {
+                        console.log(err);
+                        res.status(500).json({
+                                status: 'FAIL',
+                                message: 'erreur serveur',
+                                data: null,
+                        });
+                }
+        }
 }
